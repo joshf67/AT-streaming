8318c5b3086c7916e84d2e28e0251f30bdfadab9
joshf67 <iownall99@live.co.uk>
2018-02-09 09:26:50 +0000

    setup git

---
+++ Project/AT test/Assets/VoxelGen/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : ScriptableObject {
+
+	public static Vector3 vec3Times(Vector3 one, Vector3 two) {
+		return new Vector3 (one.x * two.x, one.y * two.y, one.z * two.z);
+	}
+
+	public static Vector3 vec3Div(Vector3 one, Vector3 two) {
+		return new Vector3 (one.x / two.x, one.y / two.y, one.z / two.z);
+	}
+
+}
